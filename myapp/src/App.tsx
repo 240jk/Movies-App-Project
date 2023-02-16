b78a9d8dcf0975ecfafcbb8f7e9b5dd9448d348c
Veronika Jones <veronikashapiro@yahoo.com>
2023-02-15 21:15:28 -0500

    search bar

---
+++ myapp/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { MovieSearch } from "./components/MovieSearch";
+
+function App() {
+  return (
+    <div className="App">
+      <MovieSearch />
+    </div>
+  );
+}
+
+export default App;
