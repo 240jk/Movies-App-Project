c8a5d91bca6f90ad081c91ddbebcaba19067ee78
Rhianon Hall <hallrhia@gmail.com>
2023-02-21 18:14:01 -0500

    added title and header and movielist components back

--- ftmovies/src/components/Header.tsx
+++ ftmovies/src/components/Header.tsx
@@ -1,9 +1,8 @@
-import '../css/Header.css'
+import "../css/Header.css";
 export function Header() {
-    return(
-        <div className="Header">
-
-        </div>
-    )
-
+  return (
+    <div className="Header">
+      <h1><i>Movie Search</i></h1>
+    </div>
+  );
 }
