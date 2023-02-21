10a9338f62a710bdb6e45ddd6c40ff6b5eb77d01
ZacharyNWilliams <zacharynwilliams9@gmail.com>
2023-02-20 18:18:48 -0800

    new adds

--- ftmovies/src/App.tsx
+++ ftmovies/src/App.tsx
@@ -6,17 +6,31 @@ import { Header } from "./components/Header";
 import ResultContextProvider from "../src/context/ResultContextProvider";
 import { Favorites } from "./components/Favorites";
 import ResultContext from "../src/context/ResultContext";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomeListRoute from "./routing/HomeListRoute";
+import FavoritesListRoute from "./routing/FavoritesListRoute";
+
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
       <ResultContextProvider>
       <Header />
       <Favorites/>
       <MovieSearch  />
-      
- 
+      <Routes>
+      <Route path="/Home" element={<HomeListRoute />} />
+        {/* <Route path="/Favorites" element = {<FavoritesListRoute/>}>
+
+          </Route>
+
+          <Route path="/Details">
+          
+          </Route> */}
+      </Routes>
       </ResultContextProvider>
+      </BrowserRouter>
     </div>
   );
 }
--- ftmovies/src/components/Header.tsx
+++ ftmovies/src/components/Header.tsx
@@ -3,6 +3,8 @@ export function Header() {
     return(
         <div className="Header">
             <h1>Movie Search</h1>
+            <button href ="/favorites" >Home</button>
+            <button>Favorites</button>
         </div>
     )
 
